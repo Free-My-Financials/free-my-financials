50d09afe015626411d475e4ae024cdad51ac3cb4
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2023-10-26 17:56:00 -0400

    Add /transaction/add page
    Co-authored-by: Grant Witty <gjwitty@bsu.edu>
    Co-authored-by: William Withers <wtwithers@bsu.edu>
    Co-authored-by:	Pandora Roberts <waroberts@bsu.edu>
    Co-authored-by: Dennis Beaver <drbeaver2@bsu.edu>

---
+++ web/src/pages/transaction/add.vue
@@ -0,0 +1,28 @@
+<template>
+<h1>
+  Add Transaction
+</h1>
+<form @submit.prevent="submit">
+  <input type="text" name="Store" id="store" v-model="store" />
+  <input type="number" name="Amount" id="amount" v-model="amount" />
+  <input type="date" name="Date" id="date" v-model="date" />
+  <input type="submit" value="submit" />
+</form>
+</template>
+
+<script lang="ts" setup>
+const store = ref("")
+const amount = ref(0)
+const date = ref(Date.now())
+
+async function submit() {
+  // const result = await useFetch("/api/transaction/add", {
+  //   method: "POST",
+  //   query: { store, amount, date }
+  // })
+  // console.log(result)
+
+  const transactions = useCookie("transactions")
+  transactions.value = transactions.value ?? "[]"
+}
+</script>
