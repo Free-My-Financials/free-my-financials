8111bb573e33f895e8f76f74aebc36b9b2f26090
wendallR <wendallar125@gmail.com>
2023-10-31 18:32:47 -0400

    Store transactions as a list in cookie

--- web/src/pages/transaction/add.vue
+++ web/src/pages/transaction/add.vue
@@ -1,3 +1,36 @@
+<script lang="ts" setup>
+
+const store = ref("")
+const amount = ref(0)
+const date = ref(Date.now())
+
+
+const transactions = useCookie(
+  "transactions",
+  {
+    default: () => [{ Store: "_", Amount: 0, Transaction_Date: 0 }]
+  }
+)
+
+async function submit() {
+  // const result = await useFetch("/api/transaction/add", {
+  //   method: "POST",
+  //   query: { store, amount, date }
+  // })
+  // console.log(result)
+  if (transactions.value && transactions.value !== null) {
+    transactions.value?.push({
+      Store: store.value,
+      Amount: amount.value,
+      Transaction_Date: date.value,
+    })
+    transactions.value = [...transactions.value]
+  }
+
+}
+</script>
+
+
 <template>
 <h1>
   Add Transaction
@@ -8,21 +41,6 @@
   <input type="date" name="Date" id="date" v-model="date" />
   <input type="submit" value="submit" />
 </form>
+<div>{{ transactions }}</div>
 </template>
 
-<script lang="ts" setup>
-const store = ref("")
-const amount = ref(0)
-const date = ref(Date.now())
-
-async function submit() {
-  // const result = await useFetch("/api/transaction/add", {
-  //   method: "POST",
-  //   query: { store, amount, date }
-  // })
-  // console.log(result)
-
-  const transactions = useCookie("transactions")
-  transactions.value = transactions.value ?? "[]"
-}
-</script>
