51f2eb4fb583591599f65b9250882651c99f20cf
PandorasGit <wendallar125@gmail.com>
2023-11-01 16:34:49 -0400

    Place transaction history in history view
    Co-authored-by: Jeffrey Harmon <contact@jeffreyharmon.dev>
    Co-authored-by: William Withers <wtwithers2020@gmail.com>
    Co-authored-by: Grant Witty <gjwitty@bsu.edu>

---
+++ web/src/pages/transaction/history.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+
+type transaction = {
+  Key: number,
+  Store: string,
+  Amount: number,
+  Transaction_Date: number
+}
+
+const transactions = useCookie(
+  "transactions",
+  {
+    default: (): transaction[] => []
+  }
+)
+
+</script>
+
+<template>
+<p v-for="transaction in transactions">
+  {{ transaction.Store }} ${{ transaction.Amount }} {{ transaction.Transaction_Date }}
+</p>
+</template>
