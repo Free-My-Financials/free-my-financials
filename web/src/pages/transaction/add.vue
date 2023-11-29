be913c3b5e07e282e4e567d33d24f9a021231f54
DennisRBeaver <drbeaver2@bsu.edu>
2023-11-28 20:50:14 -0500

    Change add page to accept custom categories, saving them via cookies

--- web/src/pages/transaction/add.vue
+++ web/src/pages/transaction/add.vue
@@ -10,7 +10,11 @@
   </UFormGroup>
 
   <UFormGroup label="Category">
-    <USelect :options="categories" v-model="state.category" />
+    <USelect :options="allCategories" v-model="state.category" />
+    <UInput v-if="state.customCategory" type="text" name="CustomCategory" id="customCategory"
+      v-model="state.customCategoryName" :key="state.customCategory.toString()" @keydown.enter.prevent
+      :placeholder="state.type === TransactionType.INCOME ? 'Source of Income' : 'Category'" />
+    <UCheckbox v-model="state.customCategory">Add custom category</UCheckbox>
   </UFormGroup>
 
   <UFormGroup label="Amount">
@@ -28,16 +32,49 @@
 
 <script lang="ts" setup>
 const transactions = useTransactions()
-const categories = ['Groceries', 'Clothing', 'Entertainment', 'Other'];
 const toast = useToast()
+const preListedCategories = ['Groceries', 'Clothing', 'Entertainment', 'Other'];
+const customCategoryKey = 'customCategories';
+const allCategories = ref([...preListedCategories, ...getCustomCategories()]);
+
 
 const state = reactive({
   store: "",
   amount: 0,
   date: '',
   type: TransactionType.EXPENSE,
-  category: categories[0]
+  category: preListedCategories[0],
+  customCategory: false,
+  customCategoryName: '',
 })
+
+function getCustomCategories() {
+  if (typeof localStorage !== 'undefined') {
+    const customCategories = localStorage.getItem(customCategoryKey);
+    return customCategories ? JSON.parse(customCategories) : [];
+  } else {
+    return [];
+  }
+}
+function addCustomCategory() {
+  const newCategory = state.customCategoryName.trim();
+  if (newCategory && !allCategories.value.includes(newCategory)) {
+    allCategories.value.push(newCategory);
+    saveCustomCategories();
+    state.category = newCategory;
+    state.customCategoryName = '';
+  }
+}
+
+function saveCustomCategories() {
+  const uniqueCustomCategories = [...new Set(allCategories.value.slice(preListedCategories.length))];
+  localStorage.setItem(customCategoryKey, JSON.stringify(uniqueCustomCategories));
+}
+
+onMounted(() => {
+  allCategories.value = [...preListedCategories, ...getCustomCategories()];
+});
+
 
 async function submit() {
   if (state.store && state.amount && state.date) {
@@ -47,8 +84,19 @@ async function submit() {
       store: state.store,
       amount: Math.round(state.amount * 100),
       date: new Date(state.date),
-      category: state.category
+      category: state.customCategory ? state.customCategoryName : state.category,
     })
+    watchEffect(() => {
+      if (state.customCategory) {
+        addCustomCategory();
+      }
+    });
+    if (state.customCategory && !preListedCategories.includes(state.customCategoryName)) {
+      preListedCategories.push(state.customCategoryName);
+      allCategories.value = [...preListedCategories, ...getCustomCategories()];
+      saveCustomCategories();
+    }
+
     resetState()
     toast.add({
       title: 'Success',
@@ -67,6 +115,8 @@ function resetState() {
   state.amount = 0
   state.date = ''
   state.type = TransactionType.EXPENSE
-  state.category = categories[0];
+  state.customCategory = false;
+  state.customCategoryName = '';
 }
+
 </script>
