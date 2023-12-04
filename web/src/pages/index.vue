0640ec008b36eaac29030eca5aa032b00b20891d
DennisRBeaver <drbeaver2@bsu.edu>
2023-12-04 00:37:19 -0500

    Remasters home screen to display current contents of site

--- web/src/pages/index.vue
+++ web/src/pages/index.vue
@@ -1,78 +1,98 @@
 <template>
-<div class="home-container">
-  <h1>Welcome to <span class="brand-text">Free My Financials</span></h1>
-  <p>
-    Free My Financials is a sophisticated web-based budgeting tool designed to shift your mindset from living
-    paycheck-to-paycheck
-    to a cashflow approach.
-  </p>
-</div>
-<div class="use-container">
-  <h1>Current Contents</h1>
-  <p>
-    Explore the features of Free My Financials to manage your finances effectively:
-  </p>
-  <ul class="feature-list">
-    <li>
-      To add transactions, visit the "<span class="green-text-link">
-        <NuxtLink to="/transaction/add">Add Transaction</NuxtLink>
-      </span>"
-      page. Input accurate information to enhance transaction tracking.
-    </li>
-    <li>
-      Analyze your transactions and current balance on the "<span class="green-text-link">
-        <NuxtLink to="/transaction/history">View Transactions</NuxtLink>
-      </span>"
-      page. Use the search bar for specific insights.
-    </li>
-    <li>
-      Create a personalized budget on the "<span class="green-text-link">
-        <NuxtLink to="/budget/editBudget">Edit Budget</NuxtLink>
-      </span>"
-      page. Tailor your budget to match your financial needs.
-    </li>
-    <li>
-      Review your budgeting information on the "<span class="green-text-link">
-        <NuxtLink to="/budget/budget">View Budget</NuxtLink>
-      </span>"
-      page, covering dates and remaining balances. Hover over the monthly balance in the top right to view the exact
-      percent change in your monthly spending.
-    </li>
-    <li>
-      Visualize your financial data on the "<span class="green-text-link">
-        <NuxtLink to="/calendar">Calendar</NuxtLink>
-      </span>" page. Track
-      spending habits and changes in monthly balances. Hover over the monthly balance in the top right to view the exact
-      percent change in your monthly spending.
-    </li>
-  </ul>
+<div class="centered-container">
+  <div class="home-container">
+    <h1>Welcome to <span class="brand-text">Free My Financials</span></h1>
+    <p>
+      Free My Financials is a sophisticated web-based budgeting tool designed to shift your mindset from living
+      paycheck-to-paycheck to a cashflow approach.
+    </p>
+    <NuxtLink to="/budget/editBudget" class="get-started-button">Get Started</NuxtLink>
+  </div>
+  <div class="use-container">
+    <h1>Current Contents</h1>
+    <p>Explore the features of Free My Financials to manage your finances effectively:</p>
+    <ul class="feature-list">
+      <li class="feature-item">
+        <h2 class="feature-title">Add transactions</h2>
+        <p class="feature-text">
+          Visit the <NuxtLink to="/transaction/add" class="green-text-link">Add Transaction</NuxtLink> page. Input
+          accurate information to enhance transaction tracking.
+        </p>
+      </li>
+      <li class="feature-item">
+        <h2 class="feature-title">Analyze transactions and balance</h2>
+        <p class="feature-text">
+          Visit the <NuxtLink to="/transaction/history" class="green-text-link">View Transactions</NuxtLink> page. Use
+          the search bar for specific insights.
+        </p>
+      </li>
+      <li class="feature-item">
+        <h2 class="feature-title">Create a personalized budget</h2>
+        <p class="feature-text">
+          Visit the <NuxtLink to="/budget/editBudget" class="green-text-link">Edit Budget</NuxtLink> page. Take control
+          of your financial future with a budget that reflects your unique lifestyle and financial goals.
+        </p>
+      </li>
+      <li class="feature-item">
+        <h2 class="feature-title">Review budgeting information</h2>
+        <p class="feature-text">
+          Visit the <NuxtLink to="/budget/budget" class="green-text-link">View Budget</NuxtLink> page, covering dates
+          and remaining balances. Hover over the monthly balance in the top right to view the exact percent change in
+          your monthly spending.
+        </p>
+      </li>
+      <li class="feature-item">
+        <h2 class="feature-title">Visualize financial data</h2>
+        <p class="feature-text">
+          Visit the <NuxtLink to="/calendar" class="green-text-link">Calendar</NuxtLink> page. Track spending habits
+          and changes in monthly balances. Hover over the monthly balance in the top right to view the exact percent
+          change in your monthly spending.
+        </p>
+      </li>
+    </ul>
+  </div>
 </div>
 </template>
 
 <style scoped>
-.home-container {
-  max-width: 800px;
-  margin: auto;
-  text-align: center;
-  padding: 40px;
+.feature-list {
+  list-style-type: none;
+  padding: 20px;
+  margin: 0;
+  margin-top: 30px;
 }
 
+.feature-item {
+  margin-bottom: 20px;
+}
+
+.feature-title {
+  font-size: 1.5em;
+  margin-bottom: 10px;
+  color: #27ae60;
+}
+
+.feature-text {
+  color: white;
+}
+
+.home-container,
 .use-container {
   max-width: 800px;
   margin: auto;
-  text-align: justify;
+  text-align: center;
   padding: 20px;
 }
 
 h1 {
   font-size: 2.5em;
   margin-bottom: 20px;
-  color: #333;
+  color: #eaeaea;
 }
 
 p {
   font-size: 1.2em;
-  color: #555;
+  color: #d7d7d7;
   line-height: 1.6;
 }
 
@@ -80,24 +100,58 @@ p {
   color: #1ca254;
 }
 
+.get-started-button {
+  display: inline-block;
+  padding: 10px 20px;
+  font-size: 1.2em;
+  background-color: #1ca254;
+  color: white;
+  border-radius: 25px;
+  text-decoration: none;
+  margin-top: 20px;
+}
+
+.get-started-button:hover {
+  background-color: #148a3d;
+}
+
+.centered-container {
+  max-width: 1200px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.use-container {
+  margin-bottom: 20px;
+  border-radius: 10px;
+  background: linear-gradient(-180deg, rgba(28, 28, 28, 0.2), rgba(39, 174, 96, 0.5));
+  color: white;
+}
+
 .feature-list {
-  list-style-type: disc;
-  /* Use 'disc' for bullet points */
   padding: 0;
-  margin-left: 20px;
-  /* Add left margin for better alignment */
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px;
 }
 
-.feature-list li {
-  margin-bottom: 10px;
+.feature-link {
+  margin-left: 15px;
+  font-weight: bold;
+  color: #27ae60;
 }
 
-.green-text-link a {
+.green-text-link {
   color: #27ae60;
   text-decoration: none;
 }
 
-.green-text-link a:hover {
+.green-text-link:hover {
   text-decoration: underline;
 }
 </style>
+
+
+
