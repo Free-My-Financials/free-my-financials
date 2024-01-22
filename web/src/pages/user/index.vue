40e480399f2268d736b26e5336b3c90fcafbddf8
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2024-01-22 15:51:52 -0500

    Fix input binding in user index.vue
    Signed-off-by: Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>

--- web/src/pages/user/index.vue
+++ web/src/pages/user/index.vue
@@ -3,14 +3,13 @@
   <UContainer :ui="{ constrained: 'max-w-4xl' }">
     <h1>Test Register/Login User</h1>
     <p>{{ auth.isLoggedIn ? auth.user?.username : "UNDEFINED" }}</p>
-    <UInput v - model=" username" />
+    <UInput v-model="username" />
     <UContainer :ui="{ constrained: 'max-w-xl' }">
       <UButton @click="auth.register(username)" block label="Register" />
       <UButton @click="auth.login(username)" block label="Login" />
       <UButton @click="auth.logout()" block label="Logout" />
     </UContainer>
   </UContainer>
-
 </div>
 </template>
 
