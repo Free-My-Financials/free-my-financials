a7c89b4fbd645f72426ebea8532df5c7e48093e4
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2024-01-23 20:46:43 -0500

    Update flake.nix to use https://flake.parts
    Signed-off-by: Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -1,49 +1,57 @@
 {
+  description = "Nix flake for Free My Financials";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devenv.shells.default = {
+          languages.javascript = {
+            enable = true;
+            package = pkgs.nodejs_20;
+          };
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
-    let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
-    {
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  packages = [
-                    pkgs.nodePackages.prisma
-                    pkgs.openssl
-                  ];
+          processes.nuxt.exec = ''
+            npm run --prefix $DEVENV_ROOT/web dev
+          '';
 
-                  env = {
-                    PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
-                    PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
-                    PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
-                    PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
-                  };
+          services.postgres = {
+            enable = true;
+            package = pkgs.postgresql;
+            initialDatabases = [{ name = "postgres"; }];
+            initialScript = "CREATE USER postgres SUPERUSER;";
+            listen_addresses = "127.0.0.1";
+          };
 
-                  languages.javascript = {
-                    enable = true;
-                    package = pkgs.nodejs_20;
-                  };
-                }
-              ];
-            };
-          });
+          scripts.prisma-init.exec = ''
+            npm run --prefix $DEVENV_ROOT/web prisma-init
+          '';
+
+          env = {
+            PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+            PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
+            PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+            PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
+
+            # Connection string for the local database
+            DATABASE_URL = "postgresql://postgres@localhost:5432/postgres";
+            JWT_SECRET = "DEV_SECRET";
+          };
+
+          packages = [
+            pkgs.nodePackages.prisma
+            pkgs.openssl
+          ] ++ (if system == "x86_64-darwin" || system == "aarch64-darwin" then [ ] else [ pkgs.glibc ]);
+        };
+      };
     };
 }
