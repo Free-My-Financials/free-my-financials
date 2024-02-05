f40b5031ecf58eaf16df747a7c25018aba73dd0d
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2024-02-05 17:37:50 -0500

    Add auth store test
    Co-authored-by: Pandora Roberts <waroberts@bsu.edu>

--- web/src/test/stores/auth.test.ts
+++ web/src/test/stores/auth.test.ts
@@ -1,29 +1,18 @@
-import { mockNuxtImport } from '@nuxt/test-utils/runtime'
-import { setActivePinia, createPinia } from 'pinia'
-import { describe, test, expect, beforeEach, vi } from 'vitest'
+import { describe, test, expect } from 'vitest'
 
 const username = 'SAM'
 
 describe('Auth Store', () => {
-  beforeEach(() => {
-    setActivePinia(createPinia())
-  })
-
-  test('user with login can login', async () => {
+  test('User isLoggedIn works', async () => {
     const auth = useAuthStore()
 
-    auth.fetchLogin = vi.fn().mockImplementation(async (name: string) => {
-      return {
-        value: {
-          token: 'Hmm?',
-          username: name,
-        }
-      }
-    })
+    expect(auth.isLoggedIn).toBe(false)
 
-    await auth.login(username)
+    auth.user = {
+      id: '1',
+      username
+    }
 
-    expect(auth.user).toBe(username)
-    // expect(auth.fetchLogin.mock.calls).toBe([])
+    expect(auth.isLoggedIn).toBe(true)
   })
 })
