7262aad4080660252023f40f80024e71d8844e78
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2024-02-05 21:50:36 -0500

    Add budget store tests and mock transaction data

--- web/src/test/stores/budget.test.ts
+++ web/src/test/stores/budget.test.ts
@@ -1,9 +1,29 @@
 import { describe, beforeEach, test, expect } from 'vitest'
+import { mockNuxtImport } from '@nuxt/test-utils/runtime'
+
+mockNuxtImport('useTransactionStore', () => {
+  return () => {
+    return {
+      transactions: [
+        { date: new Date('2024-06-15'), type: TransactionType.INCOME, amount: 100 },
+        { date: new Date('2024-06-15'), type: TransactionType.EXPENSE, amount: 50 },
+        { date: new Date('2023-12-31'), type: TransactionType.INCOME, amount: 100 },
+        { date: new Date('2025-01-01'), type: TransactionType.EXPENSE, amount: 50 },
+      ]
+    }
+  }
+})
 
 const defaultBudget = {
   amount: 0,
   startDate: new Date('2024-01-01'),
   endDate: new Date('2024-12-31'),
+}
+
+const newBudget = {
+  amount: 1000,
+  startDate: new Date('2023-01-01'),
+  endDate: new Date('2025-12-31'),
 }
 
 describe('Budget Store', () => {
@@ -21,34 +41,38 @@ describe('Budget Store', () => {
     expect(budget.endDate).toEqual(defaultBudget.endDate)
   })
 
-  test('setStartDate works', async () => {
+  test('Set budget updates budget', async () => {
     const budget = useBudgetStore()
 
-    const newDate = new Date('2023-01-01')
+    budget.setBudget(newBudget)
 
-    budget.setStartDate(newDate)
-
-    expect(budget.startDate).toEqual(newDate)
+    expect(budget.amount).toBe(newBudget.amount)
+    expect(budget.startDate).toEqual(newBudget.startDate)
+    expect(budget.endDate).toEqual(newBudget.endDate)
   })
 
-  test('setEndDate works', async () => {
+  test('Set start date updates budget start date', async () => {
     const budget = useBudgetStore()
 
-    const newDate = new Date('2025-12-31')
+    budget.setStartDate(newBudget.startDate)
 
-    budget.setEndDate(newDate)
-
-    expect(budget.endDate).toEqual(newDate)
+    expect(budget.startDate).toEqual(newBudget.startDate)
   })
 
-  test('setAmount works', async () => {
+  test('Set end date updates budget end date', async () => {
     const budget = useBudgetStore()
 
-    const newAmount = 1000
+    budget.setEndDate(newBudget.endDate)
 
-    budget.setAmount(newAmount)
+    expect(budget.endDate).toEqual(newBudget.endDate)
+  })
 
-    expect(budget.amount).toBe(newAmount)
+  test('Set amount updates budget amount', async () => {
+    const budget = useBudgetStore()
+
+    budget.setAmount(newBudget.amount)
+
+    expect(budget.amount).toBe(newBudget.amount)
   })
 
   test('Date before budget is not in budget', async () => {
@@ -85,5 +109,43 @@ describe('Budget Store', () => {
     const budget = useBudgetStore()
 
     expect(budget.transactionIsInBudget({ date: new Date('2024-06-15') })).toBe(true)
+  })
+
+  test('Transactions in budget are returned', async () => {
+    const budget = useBudgetStore()
+
+    expect(budget.transactions.length).toBe(2)
+  })
+
+  test('Total income is calculated correctly', async () => {
+    const budget = useBudgetStore()
+
+    expect(budget.totalIncome).toBe(100)
+  })
+
+  test('Total expense is calculated correctly', async () => {
+    const budget = useBudgetStore()
+
+    expect(budget.totalExpense).toBe(50)
+  })
+
+  test('Total balance is calculated correctly', async () => {
+    const budget = useBudgetStore()
+
+    expect(budget.totalBalance).toBe(50)
+  })
+
+  test('Transactions in budget update when budget is updated', async () => {
+    const budget = useBudgetStore()
+
+    expect(budget.transactions.length).toBe(2)
+
+    budget.setStartDate(new Date('2023-01-01'))
+
+    expect(budget.transactions.length).toBe(3)
+
+    budget.setEndDate(new Date('2025-12-31'))
+
+    expect(budget.transactions.length).toBe(4)
   })
 })
