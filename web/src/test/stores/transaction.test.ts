866abc7df5a99a5185e336a96319b313558668cb
PandorasGit <wendallar125@gmail.com>
2024-02-05 21:34:45 -0500

    Add transaction.test.ts

---
+++ web/src/test/stores/transaction.test.ts
@@ -0,0 +1,53 @@
+import { describe, test, expect } from 'vitest'
+
+describe('Transactions Store', () => {
+  enum TransactionType {
+    INCOME = "Income",
+    EXPENSE = "Expense",
+  }
+
+  const sampleTransaction = {
+    id: "Hi",
+    type: TransactionType.EXPENSE,
+    store: "Fortnite",
+    amount: 99,
+    date: new Date("2023-01-01"),
+    category: "Food",
+  }
+  const sampleIncome = {
+    id: "SuperHi",
+    type: TransactionType.INCOME,
+    store: "Bounty Hunting",
+    amount: 199,
+    date: new Date("2023-01-12"),
+    category: "Income",
+  }
+  const sampleSecondaryTransaction = {
+    id: "LowHi",
+    type: TransactionType.EXPENSE,
+    store: "Meat",
+    amount: 25,
+    date: new Date("2023-03-01"),
+    category: "Food",
+  }
+
+
+  test('Transaction can be added', async () => {
+    const transactions = useTransactionStore()
+
+    transactions.addTransaction(sampleTransaction)
+
+    expect(transactions.transactions.length).toBeGreaterThan(0);
+  })
+
+  test('Total Balance ', async () => {
+    const transactions = useTransactionStore()
+
+
+    expect(transactions.totalBalance).toBe(-99);
+  })
+
+
+
+
+})
