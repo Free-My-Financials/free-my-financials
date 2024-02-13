068d16ab6fe70598ba8513e4eaa8a3be7364fb2e
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2024-02-13 15:21:48 -0500

    Add in progress header

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <UINavigationMenu>
+    <UINavigationMenuList>
+      <UINavigationMenuItem>Test 1</UINavigationMenuItem>
+      <UINavigationMenuItem>Test 2</UINavigationMenuItem>
+      <UINavigationMenuItem>Test 3</UINavigationMenuItem>
+    </UINavigationMenuList>
+  </UINavigationMenu>
+</template>
