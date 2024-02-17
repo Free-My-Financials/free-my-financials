62cbc15b96bd490d071848b9e71641dbd424e2b7
PandorasGit <pandorar9000@gmail.com>
2024-02-16 20:00:51 -0500

    Cover branch where the store already exists

--- src/test/server/utils/store.test.ts
+++ src/test/server/utils/store.test.ts
@@ -1,6 +1,10 @@
 import { expect, test, vi } from 'vitest'
 import prisma from '~/server/utils/prisma/__mocks__'
-import { createStore, createOrGetStore } from '~/server/utils/prisma/store'
+import {
+  createStore,
+  createOrGetStore,
+  getStoresByUserId,
+} from '~/server/utils/prisma/store'
 
 vi.mock('~/server/utils/prisma')
 
@@ -11,6 +15,23 @@ const store = {
   updatedAt: new Date('2024-06-15'),
   userId: 'NotPandora',
 }
+
+const storeArray = [
+  {
+    id: '1984',
+    name: 'The Gun Store',
+    createdAt: new Date('2024-06-15'),
+    updatedAt: new Date('2024-06-15'),
+    userId: 'NotPandora',
+  },
+  {
+    id: 'hi there',
+    name: 'Fortnite Item Shop',
+    createdAt: new Date('2024-06-15'),
+    updatedAt: new Date('2024-06-20'),
+    userId: 'NotPandora',
+  },
+]
 
 test('Store creation should return the same store', async () => {
   prisma.store.create.mockResolvedValueOnce({ ...store })
@@ -24,8 +45,14 @@ test('createOrGetStore should return the same store if it creates the store', as
   expect(testStore).toEqual(store)
 })
 
-test('createOrGetStore should return the same store if the store exists', async () => {
+test('createOrGetStore should return the same store if it already has the store', async () => {
   prisma.store.findUnique.mockResolvedValueOnce({ ...store })
   const testStore = await createOrGetStore('NotPandora', 'The Milk Store')
   expect(testStore).toEqual(store)
+})
+
+test('getStoresByUserId should return the correct array', async () => {
+  prisma.store.findMany.mockResolvedValueOnce(storeArray)
+  const testStores = await getStoresByUserId('NotPandora')
+  expect(testStores).toEqual(storeArray)
 })
