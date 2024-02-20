2d13ae302d978e1c5298772dfd372ad5972f0359
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2024-02-20 12:02:30 -0500

    Add login/get started dialog

--- src/components/GetStartedButton.vue
+++ src/components/GetStartedButton.vue
@@ -1,3 +1,23 @@
 <template>
-  <UIButton class="bg-emerald-500">Get Started</UIButton>
+  <UIDialog>
+    <UIDialogTrigger as-child>
+      <UIButton class="bg-emerald-500">Get Started</UIButton>
+    </UIDialogTrigger>
+    <UIDialogContent class="sm:max-w-[425px]">
+      <UIDialogHeader>
+        <UIDialogTitle class="text-2xl">Sign In</UIDialogTitle>
+        <UIDialogDescription>
+          Sign in to your account to get started
+        </UIDialogDescription>
+      </UIDialogHeader>
+
+      <UIButton @click="signInWithGithub">Sign in with GitHub</UIButton>
+    </UIDialogContent>
+  </UIDialog>
 </template>
+
+<script lang="ts" setup>
+function signInWithGithub() {
+  window.location.href = '/auth/login/github'
+}
+</script>
