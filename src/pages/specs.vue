2a83f47640d2569a3b7f8338e1ebff25cd86afb3
DennisRBeaver <drbeaver2@bsu.edu>
2024-02-22 13:58:30 -0500

    Updates specs page to only display pie chart of selected month

--- src/pages/specs.vue
+++ src/pages/specs.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="page-container">
+    <div class="calendar-container">
+      <div class="calendar-nav">
+        <UButton type="button" @click="prevMonth" class="nav-button">
+          &lt;
+        </UButton>
+        <div class="month-heading">
+          <h2>{{ currentMonthName }} {{ currentYear }}</h2>
+        </div>
+        <UButton type="button" @click="nextMonth" class="nav-button">
+          &gt;
+        </UButton>
+      </div>
+    </div>
+
     <div class="charts-container">
       <canvas ref="pieChartCanvas"></canvas>
     </div>
@@ -17,16 +31,20 @@
 </template>
 
 <script setup>
-import { onMounted, ref, computed } from 'vue'
+import { onMounted, ref, computed, watch } from 'vue'
 import Chart from 'chart.js/auto'
 import { useTransactionStore } from '~/stores/transaction'
 
 const transactions = useTransactionStore()
-const deleteModalActive = ref(false)
-const deleteModalContent = ref('')
-const currentID = ref('')
-
+const currentYear = ref(new Date().getFullYear())
+const currentMonth = ref(new Date().getMonth())
+const currentMonthName = ref(
+  new Intl.DateTimeFormat('en-US', { month: 'long' }).format(
+    new Date(currentYear.value, currentMonth.value, 1)
+  )
+)
 const pieChartCanvas = ref(null)
+let chartInstance = null
 
 onMounted(() => {
   renderPieChart()
@@ -35,25 +53,35 @@ onMounted(() => {
 function renderPieChart() {
   if (pieChartCanvas.value) {
     const ctx = pieChartCanvas.value.getContext('2d')
-
     const categories = {}
     let totalAmount = 0
-
-    transactions.transactions.forEach((transaction) => {
+    const filteredTransactions = transactions.transactions.filter(
+      (transaction) => {
+        const transactionDate = new Date(transaction.date)
+        return (
+          transactionDate.getMonth() === currentMonth.value &&
+          transactionDate.getFullYear() === currentYear.value
+        )
+      }
+    )
+    filteredTransactions.forEach((transaction) => {
       if (transaction.category) {
         categories[transaction.category] =
           (categories[transaction.category] || 0) + transaction.amount
         totalAmount += transaction.amount
       }
     })
-
     const categoryLabels = Object.keys(categories)
     const categoryAmounts = Object.values(categories)
     const percentages = categoryAmounts.map(
       (amount) => (amount / totalAmount) * 100
     )
 
-    new Chart(ctx, {
+    if (chartInstance) {
+      chartInstance.destroy()
+    }
+
+    chartInstance = new Chart(ctx, {
       type: 'pie',
       data: {
         labels: categoryLabels,
@@ -78,52 +106,97 @@ function renderPieChart() {
     })
   }
 }
+
+function prevMonth() {
+  if (currentMonth.value === 0) {
+    currentMonth.value = 11
+    currentYear.value -= 1
+  } else {
+    currentMonth.value -= 1
+  }
+  updateMonthName()
+  renderPieChart()
+}
+
+function nextMonth() {
+  if (currentMonth.value === 11) {
+    currentMonth.value = 0
+    currentYear.value += 1
+  } else {
+    currentMonth.value += 1
+  }
+  updateMonthName()
+  renderPieChart()
+}
+
+function updateMonthName() {
+  currentMonthName.value = new Intl.DateTimeFormat('en-US', {
+    month: 'long',
+  }).format(new Date(currentYear.value, currentMonth.value, 1))
+}
+
+onMounted(() => {
+  updateMonthName()
+})
 </script>
 
 <style scoped>
 .page-container {
   display: flex;
+  flex-direction: column;
 }
 
-.charts-container {
-  flex: 1;
+.calendar-container {
+  background-color: #053505;
+  color: #f0f8ff;
+  border-radius: 5px;
+  margin-bottom: 20px;
+  padding: 10px;
 }
 
-.table-container {
-  flex: 1;
-  margin-right: 20px;
-}
-
-.search-container {
-  border-radius: 25px;
-  box-shadow: 0px 0px 0px;
-  width: 100%;
-  padding-top: 10px;
-  padding-left: 20px;
-  height: 2.5rem;
-  box-shadow: 0px 0px 0px;
-  width: 35%;
-  font-size: 16px;
-  line-height: 125%;
+.calendar-nav {
   display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10px;
 }
 
-.search-container input {
-  width: 100%;
-  padding-left: 40px;
-  left: -10px;
-  border-radius: 15px;
+.nav-button {
+  background-color: #006400;
+  color: #f0f8ff;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 5px;
+  cursor: pointer;
 }
 
-.search-container:hover {
-  border-radius: 25px;
+.month-heading {
+  display: flex;
+  align-items: center;
 }
 
-.search-container .search-icon {
-  position: relative;
-  left: 25px;
-  top: 5px;
-  font-size: 18px;
-  color: #555;
+.total-balance {
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+}
+
+.balance-container {
+  display: flex;
+  align-items: center;
+  margin-left: 10px;
+}
+
+.arrow {
+  margin-left: 5px;
+  font-size: 1.5em;
+}
+
+.arrow-up {
+  color: green;
+}
+
+.arrow-down {
+  color: red;
 }
 </style>
