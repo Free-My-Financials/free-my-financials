7589b4088f233c750459e3debe21299423669458
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2024-02-23 14:27:13 -0500

    Add SEO meta and HTML language attributes

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,14 @@
 <template>
   <NuxtPage />
 </template>
+
+<script lang="ts" setup>
+useSeoMeta({
+  title: 'Free My Financials',
+})
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+})
+</script>
